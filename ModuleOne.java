fe68148e96256bafcbd0747c7aa1cd5b7f5eebc3
Anna Hoerschgen <ah0959247@otc.edu>
2025-01-21 16:46:33 -0600

    Added main and some fine-tuning

--- ModuleOne.java
+++ ModuleOne.java
@@ -11,22 +11,95 @@ import java.util.Scanner;
 public class ModuleOne {
     public static void main(String[] args) {
         // instantiate reusable objects
-        Calculator calculator = new Calculator(); // Calculator
+        Calculator calculator = new Calculator(); // Calculator object
         Scanner scanner = new Scanner(System.in); // Input Scanner
+
+        // declare reusable variables
         String yesNo; // Yes/No variable, controls the loop
+        String operation; // operation type
+        int a; // first integer
+        int b; // second integer
 
         // start loop
         do {
-            System.out.println("Enter an operation: ");
-            String operation = scanner.nextLine();
+            // user selects an operation
+            System.out.println("Enter an operation: Addition, Subtraction, Multiplication, Division\n\t");
+            operation = scanner.nextLine();
 
-            System.out.println("Enter the first number: ");
-            int a = scanner.nextInt();
-            System.out.println("Enter the second number: ");
-            int b = scanner.nextInt();
+            // user chooses integers
+            System.out.println("Enter the first number: \n\t");
+            a = scanner.nextInt();
+            System.out.println("Enter the second number:\n\t");
+            b = scanner.nextInt();
 
-            // continue
+            System.out.println();
+
+            // switch statement
+            switch (operation.toLowerCase()) {
+                // using toLowerCase() for added ease of use
+                case "addition":
+                    System.out.println(
+                        a
+                        + " + "
+                        + b
+                        + " = "
+                        + calculator.add(a, b)
+                    );
+                    System.out.println("Continue operations? (yes/no)\n\t");
+                    yesNo = scanner.nextLine();
+                    break;
+                case "subtraction":
+                    System.out.println(
+                        a
+                        + " - "
+                        + b
+                        + " = "
+                        + calculator.subtract(a, b)
+                    );
+                    System.out.println("Continue operations? (yes/no)\n\t");
+                    yesNo = scanner.nextLine();
+                    break;
+                case "multiplication":
+                    System.out.println(
+                        a
+                        + " * "
+                        + b
+                        + " = "
+                        + calculator.multiply(a, b)
+                    );
+                    System.out.println("Continue operations? (yes/no)\n\t");
+                    yesNo = scanner.nextLine();
+                    break;
+                case "division":
+                    System.out.println(
+                        a
+                        + " * "
+                        + b
+                        + " = "
+                        + calculator.multiply(a, b)
+                    );
+                    System.out.println("Continue operations? (yes/no)\n\t");
+                    yesNo = scanner.nextLine();
+                    break;
+                default:
+                    System.out.println(
+                        "Invalid operation "
+                        + "\""
+                        + operation.toLowerCase()
+                        + ".\" Please try again.\nPress any key to continue..."
+                    );
+                    scanner.nextLine();
+
+                    yesNo = "yes";
+                    break;
+            }
+            System.out.println("\n\n\n");
         } while (yesNo.toLowerCase() == "yes");
+
+        // the program finishes
+        System.out.println("\n\nThank you for using the calculator! ^(Ow O)^\nPress any key to close.");
+        scanner.nextLine();
+        scanner.close();
     } // end main
 }
 
@@ -35,7 +108,7 @@ class Calculator {
         try {
             return (a + b);
         } catch (ArithmeticException e) {
-            System.out.println("Error - Arithmetic Exception - " + e.getMessage());
+            System.out.println("Error - Arithmetic Exception - " + e.getMessage() + " - Please try again.");
             return 0;
         }
     }
@@ -44,7 +117,7 @@ class Calculator {
         try {
             return (a - b);
         } catch (ArithmeticException e) {
-            System.out.println("Error - Arithmetic Exception - " + e.getMessage());
+            System.out.println("Error - Arithmetic Exception - " + e.getMessage() + " - Please try again.");
             return 0;
         }
     }
@@ -53,7 +126,7 @@ class Calculator {
         try {
             return (a * b);
         } catch (ArithmeticException e) {
-            System.out.println("Error - Arithmetic Exception - " + e.getMessage());
+            System.out.println("Error - Arithmetic Exception - " + e.getMessage() + " - Please try again.");
             return 0;
         }
     }
@@ -62,7 +135,7 @@ class Calculator {
         try {
             return (a / b);
         } catch (ArithmeticException e) {
-            System.out.println("Error - Arithmetic Exception - " + e.getMessage());
+            System.out.println("Error - Arithmetic Exception - " + e.getMessage() + " - Please try again.");
             return 0;
         }
     }
