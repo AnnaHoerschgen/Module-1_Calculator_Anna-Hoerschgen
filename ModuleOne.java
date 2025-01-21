33d76a0d4edf2a5908ac7c345c0e6efca5d7ad10
Anna Hoerschgen <ah0959247@otc.edu>
2025-01-21 12:41:56 -0600

    First Commit

---
+++ ModuleOne.java
@@ -0,0 +1,12 @@
+/*
+ * Name: Anna Hoerschgen
+ * Date: January 16th, 2025
+ * Filename: Main.java
+ * Assignment: Module 1 - Calculator
+*/
+
+public class ModuleOne {
+    public static void main(String[] args) {
+        //
+    }
+}
